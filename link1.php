2e776d9b3618381affb9975a031eeb15df9c78f3
Zingg Jorge zinggj <jorge.zingg@siemens.com>
2022-02-07 10:32:40 +0100

    first commit

---
+++ link1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Erster Link</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <h1>Das ist der erste Link.</h1>
+        <p>Als beispiel, hier wird die Page mit ein php Script generiert. Das macht nicht so viel Sinn, aber es veranschaulicht was man mit php machen kann...</p>
+        <?php echo("<h2>Das ist ein subtitel, der mit php gemacht wurde.</h2>"); ?>
+        
+        <script src="" async defer></script>
+    </body>
+</html>
